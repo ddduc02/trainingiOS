4da7680f652ed9026b666ec08d9c3cf2caa7f40a
ddduc02 <ducducdo2002@gmail.com>
2023-12-29 13:52:37 +0700

    Initial Commit

---
+++ trainingiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trainingiOS
+//
+//  Created by DucDo on 29/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
